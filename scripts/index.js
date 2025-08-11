40de4adfc2a1c74131dd1a0e253e0a75debe666f
edilmazhitov <zusupovaerkinai@mail.com>
2025-08-11 17:24:13 +0600

    Styles result-sections

--- scripts/index.js
+++ scripts/index.js
@@ -1,62 +1,11 @@
 import {transtionsWeathers} from "./transtionsWeathers.js";
-
-export { transtionsWeathers } from "./transtionsWeathers.js"
-
 const weatherForm = document.querySelector(".search-city__form");
 const weatherInput = document.querySelector(".search-city__input");
-
 const resultBlock = document.querySelector(".result__block");
 
 
-
 const apiKeys = "a6c382ccdcbfb4eed446dc20586a39f9";
-//
-//
-//
-// const getWeather = () => {
-//
-//   weatherForm.addEventListener("submit", (event) => {
-//     event.preventDefault();
-//
-//     resultBlock.innerHTML = "";
-//
-//     const cityName = weatherInput.value;
-//     if (cityName === "") {
-//       resultBlock.innerHTML = "Заполните полю";
-//       return;
-//     }
-//
-//     const API = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKeys}`;
-//
-//     fetch(API)
-//       .then(response => response.json())
-//       .then(json => {
-//         console.log('Ответ от сервера:', json)
-//         document.body.insertAdjacentHTML(
-//           'beforeend',
-//           `<pre>${JSON.stringify(json, null, 2)}</pre>`
-//         );
-//
-//         const imgIcons = json.weather[0].icon;
-//         const imgUrl = `https://openweathermap.org/img/wn/${imgIcons}@2x.png`
-//
-//         resultBlock.innerHTML = `
-//         <h2>${json.name}</h2>
-//         <img src="${imgUrl}" />
-//         `
-//       })
-//       .catch(error => {
-//         console.error('Ошибка запроса:', error)
-//       })
-//
-//     weatherInput.value = "";
-//   })
-//
-// }
-//
-//
-//
-// // getWeather();
+
 
 resultBlock.innerHTML = "";
 
@@ -86,22 +35,22 @@ const getWeatherSearch = () => {
           const imgUrl =  `https://openweathermap.org/img/wn/${res.weather[0].icon}@2x.png`
 
           const weatherDescriptions = res.weather[0].description;
-          const transletDescriptions = transtionsWeathers[weatherDescriptions] || weatherDescriptions;
-
-
+          const transletedDescriptions = transtionsWeathers[weatherDescriptions] || weatherDescriptions;
           const localTimes = ts => new Date(ts * 1000 ).toLocaleTimeString("ru-RU", {hour: "2-digit", minute: "2-digit"})
 
 
 
 
           resultBlock.innerHTML = `
+              <h2 class="result__title">Погода</h2>
              <img src="${imgUrl}" class="result__images" />
-             <h2 class="result__city-name">${res.name} ${res.sys.country}</h2>
-             <h3 class="result__descriptions">${transletDescriptions}</h3>
+             <h2 class="result__city-name">${res.name} <span>${res.sys.country}</span></h2>
+             <h3 class="result__descriptions">${transletedDescriptions}</h3>
              <h3 class="result__degree">${Math.round(res.main.temp - 273,15)}°С</h3>
+             <h3 class="result__humidity">Влажность: <span>${res.main.humidity}%</span></h3>
              <div class="result__times">
-                <h4>Восход солнце: ${localTimes(res.sys.sunrise)}</h4>  
-                <h4>Закат солнце: ${localTimes(res.sys.sunset)}</h4>  
+                <h4 class="result__times-text">Восход солнце: <span>${localTimes(res.sys.sunrise)}</span></h4>  
+                <h4 class="result__times-text">Закат солнце: <span>${localTimes(res.sys.sunset)}</span></h4>  
              </div>
              
           `
