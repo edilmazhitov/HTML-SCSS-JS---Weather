9cf7d2797cedad365cff013b60968621826f1ae0
edilmazhitov <zusupovaerkinai@mail.com>
2025-08-12 12:38:00 +0600

    FINISH PROJECTS

--- scripts/burgerMenu.js
+++ scripts/burgerMenu.js
@@ -1,11 +1,10 @@
-const root = document.getElementById("header");
-const overlay = document.getElementById("burger-overlay");
-const burgerButton = document.getElementById("burger-btn");
+const root = document.getElementById('header')
+const overlay = document.getElementById('burger-overlay')
+const burgerButton = document.getElementById('burger-btn')
 
-
-burgerButton.addEventListener("click", () => {
+burgerButton.addEventListener('click', () => {
   burgerButton.classList.toggle('is-active')
   overlay.classList.toggle('is-active')
 
-  document.documentElement.classList.toggle("is-lock")
+  document.documentElement.classList.toggle('is-lock')
 })
--- scripts/index.js
+++ scripts/index.js
@@ -1,52 +1,49 @@
-import {transtionsWeathers} from "./transtionsWeathers.js";
-const weatherForm = document.querySelector(".search-city__form");
-const weatherInput = document.querySelector(".search-city__input");
-const resultBlock = document.querySelector(".result__block");
+import { transtionsWeathers } from './transtionsWeathers.js'
+const weatherForm = document.querySelector('.search-city__form')
+const weatherInput = document.querySelector('.search-city__input')
+const resultBlock = document.querySelector('.result__block')
 
+const apiKeys = 'a6c382ccdcbfb4eed446dc20586a39f9'
 
-const apiKeys = "a6c382ccdcbfb4eed446dc20586a39f9";
+resultBlock.innerHTML = ''
 
-
-resultBlock.innerHTML = "";
-
-resultBlock.style.display = "none"
-
+resultBlock.style.display = 'none'
 
 const getWeatherSearch = () => {
-  weatherForm.addEventListener("submit", (event) => {
-    event.preventDefault();
+  weatherForm.addEventListener('submit', (event) => {
+    event.preventDefault()
     try {
-      resultBlock.innerHTML = "";
+      resultBlock.innerHTML = ''
 
-      const weatherSearchCityValue = weatherInput.value;
-      if (weatherSearchCityValue === "") {
-        throw new Error("Заполните поля поиска города")
+      const weatherSearchCityValue = weatherInput.value
+      if (weatherSearchCityValue === '') {
+        throw new Error('Заполните поля поиска города')
       }
 
       const API = `https://api.openweathermap.org/data/2.5/weather?q=${weatherSearchCityValue}&appid=${apiKeys}&lang=ru`
 
-
-      resultBlock.style.display = "block";
+      resultBlock.style.display = 'block'
 
       fetch(API)
-        .then(responce => responce.json())
-        .then(res => {
+        .then((responce) => responce.json())
+        .then((res) => {
+          const imgUrl = `https://openweathermap.org/img/wn/${res.weather[0].icon}@2x.png`
 
-          const imgUrl =  `https://openweathermap.org/img/wn/${res.weather[0].icon}@2x.png`
-
-          const weatherDescriptions = res.weather[0].description;
-          const transletedDescriptions = transtionsWeathers[weatherDescriptions] || weatherDescriptions;
-          const localTimes = ts => new Date(ts * 1000 ).toLocaleTimeString("ru-RU", {hour: "2-digit", minute: "2-digit"})
-
-
-
+          const weatherDescriptions = res.weather[0].description
+          const transletedDescriptions =
+            transtionsWeathers[weatherDescriptions] || weatherDescriptions
+          const localTimes = (ts) =>
+            new Date(ts * 1000).toLocaleTimeString('ru-RU', {
+              hour: '2-digit',
+              minute: '2-digit',
+            })
 
           resultBlock.innerHTML = `
               <h2 class="result__title">Погода</h2>
              <img src="${imgUrl}" class="result__images" />
              <h2 class="result__city-name">${res.name} <span>${res.sys.country}</span></h2>
              <h3 class="result__descriptions">${transletedDescriptions}</h3>
-             <h3 class="result__degree">${Math.round(res.main.temp - 273,15)}°С</h3>
+             <h3 class="result__degree">${Math.round(res.main.temp - 273, 15)}°С</h3>
              <h3 class="result__humidity">Влажность: <span>${res.main.humidity}%</span></h3>
              <div class="result__times">
                 <h4 class="result__times-text">Восход солнце: <span>${localTimes(res.sys.sunrise)}</span></h4>  
@@ -56,9 +53,9 @@ const getWeatherSearch = () => {
           `
         })
 
-      weatherInput.value = "";
+      weatherInput.value = ''
     } catch (err) {
-      resultBlock.style.display = "block";
+      resultBlock.style.display = 'block'
       resultBlock.innerHTML = `
           <h1 class="errors">
             Ошибка при поиске: ${err.message}
@@ -68,6 +65,4 @@ const getWeatherSearch = () => {
   })
 }
 
-
-
-getWeatherSearch();
+getWeatherSearch()
